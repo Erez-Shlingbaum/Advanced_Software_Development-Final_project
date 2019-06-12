e84177e3acbacd192f3495732e716142f16cded7
shaked.tayouri <shaked.tayouri@gmail.com>
2019-06-12 23:41:30 +0300

    add a IView interface start the structure of the controller

---
+++ PTM2_Interpeter/src/View/IView.java
@@ -0,0 +1,9 @@
+package View;
+
+import ViewModel.ViewModel;
+
+public interface IView
+{
+    public void setViewModel(ViewModel vm);
+    public void onCalc();
+}
--- PTM2_Interpeter/src/View/MainWindowController.java
+++ PTM2_Interpeter/src/View/MainWindowController.java
@@ -1,7 +1,28 @@
 package View;
 
+import ViewModel.ViewModel;
 
-public class MainWindowController
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainWindowController implements Observer, IView
 {
 
+    @Override
+    public void setViewModel(ViewModel vm)
+    {
+
+    }
+
+    @Override
+    public void onCalc()
+    {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg)
+    {
+
+    }
 }
